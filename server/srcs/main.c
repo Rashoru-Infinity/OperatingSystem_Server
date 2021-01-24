bdfbb64b8409ec3f6528bb3d7db61806e39ae24a
khagiwar <khagiwar@student.42tokyo.jp>
2021-01-24 19:14:16 +0900

    modified:   server/srcs/main.c

--- server/srcs/main.c
+++ server/srcs/main.c
@@ -8,6 +8,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <pthread.h>
+
+typedef struct	s_update {
+	int		*pipefd;
+	t_hash	*name_list;
+	char	*buf;
+	int		buf_size;
+	int		new_sockfd;
+}				t_update;
+
+typedef struct	s_upload {
+	int			*pipefd;
+	char		*buf;
+	int			buf_size;
+	int			new_sockfd;
+	char		*home;
+	t_hash		*name_list;
+}				t_upload;
+
+typedef struct	s_accept {
+	int					*new_sockfd;
+	int					sockfd;
+	struct sockaddr_in	*clnt;
+	socklen_t			*sin_siz;
+}				t_accept;
 
 void send_dir(const char *home, char *buf, int buf_size, int new_sockfd, t_hash *namelist)
 {
@@ -60,28 +85,141 @@ void authorize(char *buf, int buf_size, int new_sockfd, const char *home)
 		exit(1);
 }
 
-void main_communication(char *buf, int buf_size, int new_sockfd, char *home)
+void accept_wrapper(void *arg)
 {
-	char *repodir;
+	t_accept	args;
 
-	bzero(buf, buf_size + 1);
-	while (recv(new_sockfd, buf, buf_size + 1, 0) > 0)
+	if (!arg)
+		return ;
+	args = (t_accept *)arg;
+	if (!args->new_sockfd || !args->clnt || !args->sin_siz)
+		return ;
+	*(args->new_sockfd)
+	= accept(args->sockfd, (struct sockaddr *)&args->clnt, args->sin_siz);
+}
+
+void reflect_change(void *arg)
+{
+	t_update	*args;
+	size_t		list_size;
+
+	if (!arg)
+		return ;
+	args = (t_update *)arg;
+	if (!args->name_list || args->name_list->vla.real_size == 0 || !args->buf)
+		return ;
+	list_size = hash_size(*(args->name_list));
+	bzero(args->buf, args->buf_size + 1);
+	if (read(pipefd[0], args->buf, args->buf_size) < 0)
+		return ;
+	write(pipefd[1], args->buf, args->buf_size);
+	hash_add(args->name_list, args->buf);
+}
+
+void update_dir(void *arg)
+{
+	t_update	*args;
+	size_t		list_size;
+
+	if (!arg)
+		return ;
+	args = (t_update *)arg;
+	if (!args->name_list || args->name_list->vla.real_size == 0 || !args->buf)
+		return ;
+	list_size = hash_size(*(args->name_list));
+	bzero(args->buf, args->buf_size + 1);
+	while (read(pipefd[0], args->buf, args->buf_size) > 0)
+	{
+		if (hash_add(args->name_list, args->buf) == fail)
+			return ;
+		if (list_size != hash_size(*(args->name_list)))
+		{
+			if (send(args->new_sockfd, args->buf, args->buf_size + 1, 0) < 0)
+				exit(1);
+			list_sizse = hash_size(*(args->name_list));
+		}
+		bzero(args->buf, args->buf_size + 1);
+	}
+}
+
+void upload_dir(void *arg)
+{
+	t_upload	*args;
+	char		*repodir;
+	char		*fullpath;
+
+	if (!arg)
+		return ;
+	args = arg;
+	if (!args->buf || !args->home || !args->name_list || !args->pipefd)
+		return ;
+	bzero(args->buf, args->buf_size + 1);
+	while (recv(args->new_sockfd, args->buf, args->buf_size + 1, 0) > 0)
 	{
 		repodir = NULL;
-		buf[buf_size] = '\0';
-		if (strcmp(buf, "exit") == 0)
+		fullpath = NULL;
+		args->buf[args->buf_size] = '\0';
+		if (strcmp(args->buf, "exit") == 0)
 			exit(0);
-		if (is_safestr(buf))
-			repodir = gen_repository(home, buf);
-		bzero(buf, buf_size + 1);
-		if (repodir)
-			strcpy(buf, repodir);
-		else
-			strcpy(buf, "fail to generate repository");
-		if (send(new_sockfd, buf, buf_size + 1, 0) < 0)
+		if (is_safestr(args->buf))
+		{
+			if (!(fullpath = strargcat(4, args->home, "/repo/", args->buf, ".git")))
+				return ;
+			if (!hash_contains(args->name_list, fullpath))
+			{
+				repodir = gen_repository(args->home, args->buf);
+				bzero(args->buf, args->buf_size + 1);
+				if (repodir)
+				{
+					write(pipefd[1], fullpath, strlen(fullpath));
+					hash_add(args->name_list, fullpath);
+					fullpath = NULL;
+					strcpy(args->buf, repodir);
+					if (send(args->new_sockfd, args->buf, args->buf_size + 1, 0) < 0)
+						exit(1);
+					bzero(args->buf, args->buf_size + 1);
+					free(repodir);
+					continue ;
+				}
+			}
+		}
+		strcpy(args->buf, "fail to generate repository");
+		if (send(args->new_sockfd, args->buf, args->buf_size + 1, 0) < 0)
 			exit(1);
-		bzero(buf, buf_size + 1);
 		free(repodir);
+		free(fullpath);
+	}
+}
+
+void	main_communication(char *buf, int buf_size, int new_sockfd,
+		char *home, t_hash *namelist, int *pipefd)
+{
+	t_update	update;
+	t_upload	upload;
+	pthread_t	update_th;
+	pthread_t	upload_th;
+
+	update = (t_update){pipefd, name_list, buf, buf_size};
+	upload = (t_upload){pipefd, buf, buf_size, new_sockfd, home, name_list};
+	if (pthread_create(&update_th, NULL, (void *)update_dir, (void *)&update))
+	{
+		perror("pthread_create\n");
+		exit(1);
+	}
+	if (pthread_create(&upload_th, NULL, (void *)upload_dir, (void *)&upload))
+	{
+		perror("pthread_create\n");
+		exit(1);
+	}
+	if (pthread_join(update_th, NULL))
+	{
+		perror("pthread_join\n");
+		exit(1);
+	}
+	if (pthread_join(upload_th, NULL))
+	{
+		perror("pthread_join\n");
+		exit(1);
 	}
 }
 
@@ -97,6 +235,9 @@ int main(int argc, char **argv)
 	int					status;
 	t_hash				name_list;
 	int					pipefd[2];
+	t_accept			accept_args;
+	pthread_t			accept_wrapper_th;
+	pthread_t			reflect_change_th;
 
 	if (argc != 2)
 	{
@@ -133,6 +274,7 @@ int main(int argc, char **argv)
 		perror("listen\n");
 		exit(1);
 	}
+	accept_args = (t_accept){&new_sockfd, sockfd, &clnt, &sin_siz};
 	home = getenv("HOME");
 	if (daemon(0, 0) != 0)
 	{
@@ -144,8 +286,18 @@ int main(int argc, char **argv)
 		perror("pipe\n");
 		exit(1);
 	}
+	if (pthread_cread(&reflect_change_th, NULL, (void *)reflect_change, (void *)&accept_args))
+	{
+		perror("pthread_create\n");
+		exit(1);
+	}
 	while (1)
 	{
+		if (pthread_cread(&accept_wrapper_th, NULL, (void *)reflect_change, (void *)&accept_args))
+		{
+			perror("pthread_create\n");
+			exit(1);
+		}
 		if ((new_sockfd = accept(sockfd, (struct sockaddr *)&clnt, &sin_siz)) < 0)
 			perror("accept\n");
 		switch((pid = fork()))
@@ -155,7 +307,7 @@ int main(int argc, char **argv)
 			{
 			case 0:
 				authorize(buf, size, new_sockfd, home);
-				send_dir(home, buf, DEFAULT_SIZE, new_sockfd, namelist);
+				send_dir(home, buf, DEFAULT_SIZE, new_sockfd, name_list);
 				main_communication(buf, buf_size, new_sockfd, home);
 				exit(0);
 			case -1:
@@ -171,5 +323,8 @@ int main(int argc, char **argv)
 		close(new_sockfd);
 	}
 	close(sockfd);
+	close(pipefd[0]);
+	close(pipefd[1]);
+	hash_clear(&name_list, false, true);
 	return (0);
 }
