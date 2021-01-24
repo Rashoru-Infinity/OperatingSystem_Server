090197a6b0c7a6cab8e4c890d68d149c3e2f736d
khagiwar <khagiwar@student.42tokyo.jp>
2021-01-24 12:34:54 +0900

    completed check_dir

---
+++ server/srcs/check_dir.c
@@ -0,0 +1,26 @@
+#include "server.h"
+#include <dirent.h>
+#include <stdlib.h>
+
+t_status	check_dir(t_hash *file_list, const char *path)
+{
+	DIR				*dir;
+	struct dirent	*dp;
+
+	if (!file_list || !path)
+		return (fail);
+	if (!(dir = opendir(path)))
+		return (fail);
+	dp = readdir(dir);
+	while (dp)
+	{
+		if (hash_add(file_list, dp->d_name) == fail)
+		{
+			closedir(dir);
+			return (fail);
+		}
+		dp = readdir(dir);
+	}
+	closedir(dir);
+	return (success);
+}
